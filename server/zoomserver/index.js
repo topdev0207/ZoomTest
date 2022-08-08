7d1daea706f56236bdd503ec706c15aad16dfd20
topdev0207 <topdev0207@gmail.com>
2022-08-08 22:01:41 +1000

    test changes

--- server/zoomserver/index.js
+++ server/zoomserver/index.js
@@ -13,12 +13,18 @@ const app = express()
 app.use(cors())
 
 // Allow CORS access
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
-  if (req.method == "OPTIONS") {
-    return res.sendStatus(200);
-  }
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+//   if (req.method == "OPTIONS") {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
