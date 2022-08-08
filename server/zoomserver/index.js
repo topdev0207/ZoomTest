d9c56f9eec37bfa12014d9f34ccbdb3359a83a40
topdev0207 <topdev0207@gmail.com>
2022-08-08 22:18:06 +1000

    test changes

--- server/zoomserver/index.js
+++ server/zoomserver/index.js
@@ -10,8 +10,8 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 
-//app.use(cors())
-app.use(cors({origin: '*'}));
+app.use(cors())
+// app.use(cors({origin: '*'}));
 
 // Allow CORS access
 // app.use((req, res, next) => {
@@ -23,11 +23,11 @@ app.use(cors({origin: '*'}));
 //   next();
 // });
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // Call Zoom auth API
 app.get('/', (req, res) => {
