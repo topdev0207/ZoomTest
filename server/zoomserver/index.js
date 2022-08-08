2444a50c1a149402c4807b59831c3e9820448162
topdev0207 <topdev0207@gmail.com>
2022-08-08 22:22:13 +1000

    test changes

--- server/zoomserver/index.js
+++ server/zoomserver/index.js
@@ -23,6 +23,14 @@ app.use(cors())
 //   next();
 // });
 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
